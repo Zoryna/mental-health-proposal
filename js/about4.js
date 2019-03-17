89da5fa5922292dfb7a183687abe1cc9c0e0cb5f
Zoryna <jsoryna@gmail.com>
2019-03-17 18:59:15 -0500

    Fixed the stucture of the questins and answers

--- js/about4.js
+++ js/about4.js
@@ -1,14 +1,14 @@
-var coll = document.getElementsByClassName("question");
+var ques = document.getElementsByClassName("question");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+for (i = 0; i < ques.length; i++) {
+  ques[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var answer = this.nextElementSibling;
-    if (answer.display === "block") {
-      answer.display = "none";
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
     } else {
-      answer.display = "block";
+      answer.style.display = "block";
     }
   });
 }

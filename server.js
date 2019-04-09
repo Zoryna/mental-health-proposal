4b8fa98d1c74363835ab73e10efa8de5a0948a67
Zoryna <jsoryna@gmail.com>
2019-04-09 18:12:15 -0500

    Included the other html files for the server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "./", "index.html")));
 
+app.get("/index.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "index.html"))
+);
+
 app.get("/about1.html", (req, res) =>
   res.sendFile(path.join(__dirname, "./", "about1.html"))
 );
@@ -13,11 +17,24 @@ app.get("/about2.html", (req, res) =>
   res.sendFile(path.join(__dirname, "./", "about2.html"))
 );
 
+app.get("/about3.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "about3.html"))
+);
 
+app.get("/about4.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "about4.html"))
+);
 
+app.get("/getinvolved.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "getinvolved.html"))
+);
 
+app.get("/signup.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "signup.html"))
+);
 
-
-
+app.get("/contact.html", (req, res) =>
+  res.sendFile(path.join(__dirname, "./", "contact.html"))
+);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
